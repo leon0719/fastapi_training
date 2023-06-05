24c74718f428fb6945448baf02267c779c3fae96
leon0719 <leon0970865551@gmail.com>
2023-06-06 00:17:30 +0800

    initial

--- test.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-
-db = client["school"]
-collection = db["students"]
-query = { "name": "John" }
-print(collection.find_one(query))
