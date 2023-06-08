fd3035979c4e44d8577ebcb476918daa88bbd901
leon0719 <leon0970865551@gmail.com>
2023-06-08 20:33:00 +0800

    1

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from typing import Union
-from models.model import User
-from fastapi import FastAPI, Query, Path
+
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.post("/users")
-def create_user(user: User):
-    # 在這裡可以使用驗證過的 user 物件
-    # 例如，儲存到資料庫或進行其他操作
-    return {"message": "User created successfully"}
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 
-@app.get("/items/{item_id}")
-async def read_items(
-    item_id: int = Path(title="The ID of the item to get"),
-    q: Union[str, None] = Query(default=None, alias="item-query"),
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int,
+                      item: Item,
+                      user: User,
+                      importance: int = Body()):
+    results = {"item_id": item_id,
+               "item": item,
+               "user": user,
+               "importance": importance}
     return results
