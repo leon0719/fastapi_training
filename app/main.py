45aa83afb36014fdfa8d9aa1ac8b874e1648f0de
leon0719 <leon0970865551@gmail.com>
2023-06-14 17:55:29 +0800

    update

--- app/main.py
+++ app/main.py
@@ -1,24 +1,4 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
-app = FastAPI()
-
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/")
-async def main():
-    return {"message": "Hello World"}
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", log_level="info")
