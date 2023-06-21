490fcb2123a9651f22555d19285a51020c9cdea4
leon0719 <leon0970865551@gmail.com>
2023-06-21 14:19:01 +0800

    update

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from database import Base
 from sqlalchemy import Column, Integer, Boolean
 from sqlalchemy import String, TIMESTAMP, text
+from sqlalchemy import ForeignKey
 
 
 class Post(Base):
@@ -8,10 +9,11 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default="TRUE", nullable=False)
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 
 class User(Base):
